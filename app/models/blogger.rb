523c050df8c7f55a6b279dff3b190d13742aeb9c
Justin Hutsell <hutselljustin@gmail.com>
2019-07-02 17:57:33 -0400

    awesome app

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,27 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+
+    validates :name, presence: true, uniqueness: true
+    validates :age, presence: true, numericality: { :greater_than => 0 }
+    validates :bio, presence: true, numericality: { :greater_than => 30 }
+
+    def total_likes 
+        total_likes = 0 
+        self.posts.each do |post|
+            total_likes += post.likes
+        end
+        total_likes
+    end
+
+    def featured_post 
+        feat_post = self.posts[0]
+        self.posts.each do |post| 
+            if post.likes > feat_post.likes
+                feat_post = post 
+            end
+        end
+        feat_post
+    end
 
 end
